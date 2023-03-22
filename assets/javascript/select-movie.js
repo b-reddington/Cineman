aaf9811d894bc147e4578d66cf9b5832070cfca8
Grace Roh <grace@BIGMINE.attlocal.net>
2023-03-22 12:47:09 -0700

    create javascript file for selecting movies functionalities & pseudocode

---
+++ assets/javascript/select-movie.js
@@ -0,0 +1,11 @@
+//html form elements -- vanilla javascript***
+
+//variables
+
+//api links
+
+//fetch -- get a list of movies & store selected movie into variable
+
+//fetch -- get detailed information about a movie & render onto page
+
+//click event -- 'submit', render movie onto page
