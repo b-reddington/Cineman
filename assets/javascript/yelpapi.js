d075bb25ceeb51d96fc31099283220a964073220
Connor Raper-Mateo <connor.rapermateo@student.csulb.edu>
2023-03-26 15:26:21 -0700

    rearranged code

--- assets/javascript/yelpapi.js
+++ assets/javascript/yelpapi.js
@@ -6,11 +6,11 @@ function yelpApi() {
         location: $("#location").val().trim(), // Grabs the location of an area the user types in
         foods: ['american', 'chinese', 'mexican', 'japanese', 'indian', 'italian', 'korean'],
         foodSplit: [],
-        limit: 1, // How many results appear
+        limit: 15, // How many results to fetch
         delivery: 'restaurants_delivery', // Select restaurants that offer delivery
     }
-
     let selectedFoods = [];
+
     function isChecked() {
         for (i = 0; i < select.foods.length; i++) {
             select.foodSplit.push(select.foods[i].split(' '));
@@ -26,44 +26,54 @@ function yelpApi() {
     &categories=${selectedFoods} // Selects what type of food you prefer
     &location=${select.location} // Selects restaurants near the designated address
     &attributes=${select.delivery} // Select only restaurants that deliver
-    &limit=${select.limit}`, { // Select 1 restaurant from the list
+    &limit=${select.limit}`, { // Select 10 restaurants from the list
         headers: {
             Authorization: apiKey,
         },
     })
         .then((response) => response.json())
         .then((data) => {
-            // Print the names of the businesses that were retrieved
-            data.businesses.forEach((business) => {
-                console.log(
-                    "Name: " + business.name + // Displays Business Name
-                    "\nRating: " + business.rating + // Displays Selected Business Rating
-                    "\nPrice Range: " + business.price + // Displays Selected Business Price Range
-                    "\nCategories: " + JSON.stringify(select.foods) + // Displays categories of business
-                    "\nImg URL: " + business.image_url + // Displays a featured image URL from the business
-                    "\nAddress: " + JSON.stringify(business.location) + // Displays the address of a business
-                    "\nYelp Link: " + business.url); // Displays the link to the yelp business
-                $(".foodResults").prepend("<img id='foodImg' src='" + business.image_url + "'>"); // Inserts an image from Yelp Object
-                $("#restaurantName").text(business.name); // Displays Business Name
-                $("#restaurantType").text(selectedFoods);  // Displays Selected Business Rating
-                $("#restaurantRating").text("Rating: " + business.rating + "/5");  // Displays Selected Business Rating
-                $("#restaurantPrice").text("Average Price: " + business.price); // Displays Selected Business Price Range
-                console.log(selectedFoods)
+            // Randomly pick one restaurant from the list
+            const randomIndex = Math.floor(Math.random() * data.businesses.length);
+            const business = data.businesses[randomIndex];
+            $(".foodResults").prepend("<img id='foodImg' src='" + business.image_url + "'>"); // Inserts an image from Yelp Object
+            $("#restaurantName").text(business.name); // Displays Business Name
+            $("#restaurantType").text(selectedFoods);  // Displays Selected Business Rating
+            $("#restaurantRating").text("Rating: " + business.rating + "/5");  // Displays Selected Business Rating
+            $("#restaurantPrice").text("Average Price: " + business.price); // Displays Selected Business Price Range
+
+            let navButton = $("<button>");
+            navButton.text("Order from Here");
+            navButton.attr("id", "order");
+            navButton.attr("class", "button is-centered is-success is-medium")
+            $(".afterBtns").append(navButton)
+            $('#order').click(function () {
+                window.open(business.url, '_blank');
             });
+
+            const savedInfo = { // A variable for saving generated data
+                image: business.image_url,
+                name: business.name,
+                type: selectedFoods,
+                rating: business.rating,
+                price: business.price
+            };
+            const stringInfo = JSON.stringify(savedInfo); // Converts saved data as a string
+            let saveButton = $("<button>");
+            saveButton.text("Save Results");
+            saveButton.attr("id", "saveFood");
+            saveButton.attr("class", "button is-centered is-success is-medium");
+            $(".afterBtns").append(saveButton)
+            $("#saveFood").click(function () {
+                localStorage.setItem('Generated Food', stringInfo);
+            })
+
         })
         .catch((error) => console.log(error));
 }
-$("#generate").click(function () {
-    $(".foodResults img").remove(); // remove images when searcching for new restaurants to avoid creating multiple images
-    yelpApi();
-    $('.foodResults').css('display', 'block');
-    
-});
 $("#generateFood").click(function () {
     $(".foodResults img").remove(); // remove images when searcching for new restaurants to avoid creating multiple images
-    yelpApi();
+    $("#saveFood").remove();
     $('.foodResults').css('display', 'block');
-    console.log(genFood)
+    yelpApi();
 });
-
-
