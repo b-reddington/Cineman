020feb2d2dcb1020fa94683a4b17dc7993be80a3
b-reddington <brandon.reddington98@gmail.com>
2023-03-26 14:04:19 -0700

    Removed unnecessary console logs, added a save button, additional filtering

--- assets/javascript/yelpapi.js
+++ assets/javascript/yelpapi.js
@@ -9,8 +9,8 @@ function yelpApi() {
         limit: 15, // How many results to fetch
         delivery: 'restaurants_delivery', // Select restaurants that offer delivery
     }
-
     let selectedFoods = [];
+
     function isChecked() {
         for (i = 0; i < select.foods.length; i++) {
             select.foodSplit.push(select.foods[i].split(' '));
@@ -36,27 +36,43 @@ function yelpApi() {
             // Randomly pick one restaurant from the list
             const randomIndex = Math.floor(Math.random() * data.businesses.length);
             const business = data.businesses[randomIndex];
-            console.log(
-                "Name: " + business.name + // Displays Business Name
-                "\nRating: " + business.rating + // Displays Selected Business Rating
-                "\nPrice Range: " + business.price + // Displays Selected Business Price Range
-                "\nCategories: " + JSON.stringify(select.foods) + // Displays categories of business
-                "\nImg URL: " + business.image_url + // Displays a featured image URL from the business
-                "\nAddress: " + JSON.stringify(business.location) + // Displays the address of a business
-                "\nYelp Link: " + business.url); // Displays the link to the yelp business
             $(".foodResults").prepend("<img id='foodImg' src='" + business.image_url + "'>"); // Inserts an image from Yelp Object
             $("#restaurantName").text(business.name); // Displays Business Name
             $("#restaurantType").text(selectedFoods);  // Displays Selected Business Rating
             $("#restaurantRating").text("Rating: " + business.rating + "/5");  // Displays Selected Business Rating
             $("#restaurantPrice").text("Average Price: " + business.price); // Displays Selected Business Price Range
-            console.log(selectedFoods);
+
+            let navButton = $("<button>");
+            navButton.text("Order from Here");
+            navButton.attr("id", "order");
+            navButton.attr("class", "button is-centered is-success is-medium")
+            $(".afterBtns").append(navButton)
+            $('#order').click(function () {
+                window.open(business.url, '_blank');
+            });
+
+            const savedInfo = { // A variable for saving generated data
+                image: business.image_url,
+                name: business.name,
+                type: selectedFoods,
+                rating: business.rating,
+                price: business.price
+            };
+            const stringInfo = JSON.stringify(savedInfo); // Converts saved data as a string
+            let saveButton = $("<button>");
+            saveButton.text("Save Results");
+            saveButton.attr("id", "saveFood");
+            saveButton.attr("class", "button is-centered is-success is-medium");
+            $(".afterBtns").append(saveButton)
+            $("#saveFood").click(function () {
+                localStorage.setItem('Generated Food', stringInfo);
+            })
+
         })
         .catch((error) => console.log(error));
 }
 $("#generateFood").click(function () {
     $(".foodResults img").remove(); // remove images when searcching for new restaurants to avoid creating multiple images
+    $("#saveFood").remove();
     yelpApi();
 });
-
-
-
